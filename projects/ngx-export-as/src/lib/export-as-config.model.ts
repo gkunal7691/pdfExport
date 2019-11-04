3584156f9096e8507d77e3b28a30fc4a39cac163
Gaurav Kunal <gkunal7691@gmail.com>
2019-11-04 17:24:34 +0530

    pdf in landscape mode

--- projects/ngx-export-as/src/lib/export-as-config.model.ts
+++
@@ -1,9 +0,0 @@
-export type SupportedExtensions = 'pdf' | 'png' | 'xlsx' | 'xls' | 'docx' | 'doc' | 'txt' | 'csv' | 'json' | 'xml';
-
-export interface ExportAsConfig {
-  type: SupportedExtensions;
-  elementId: string;
-  download?: boolean;
-  fileName?: string;
-  options?: any;
-}
